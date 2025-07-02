ff3f7b1332a63d13e8c1d87c851cd0f3023082c5
KIMSOSLE <pdw05027@gmail.com>
2025-07-02 17:45:01 +0900

    게시물에 번호 지정하기, 답변 개수 표시하기, 스프링시큐리티 설정하기, 회원가입 기능 구현하기

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.HeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration // @Configuration → 해당 클래스가 스프링 설정 클래스임을 의미
+@EnableWebSecurity // @EnableWebSecurity → 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 설정
+public class SecurityConfig {
+
+	@Bean // @Bean → Spring 컨테이너에 객체를 직접 등록할 때 사용
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // 모든 페이지에 접근 가능
+				// permitAll() → 특정 경로에 대한 모든 요청을 허용
+				.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))) // h2-console을
+																											// 보안에서 허용
+				.headers((headers) -> headers.addHeaderWriter((HeaderWriter) new XFrameOptionsHeaderWriter(
+						XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+		return http.build();
+
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb.user;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+	private final UserRepository userRepository;
+
+	private final PasswordEncoder passwordEncoder;
+
+	public SiteUser create(String username, String password, String email) {
+		SiteUser user = new SiteUser();
+		user.setUsername(username);
+		user.setEmail(email);
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		// @Bean으로 등록된 메서드, 객체 주입을 받았기 때문에 사용 안 함
+		user.setPassword(passwordEncoder.encode(password));
+		this.userRepository.save(user);
+		return user;
+	}
+}
