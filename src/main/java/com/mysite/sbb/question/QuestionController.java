6c259af4a342271367aa904ddbf3e2106f56157d
KIMSOSLE <pdw05027@gmail.com>
2025-07-09 16:47:28 +0900

    수정과 삭제 기능 추가하기, 질문과 답변 수정 일시 표시하기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.question;
 import java.security.Principal;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.mysite.sbb.answer.AnswerForm;
 import com.mysite.sbb.user.SiteUser;
@@ -75,5 +77,46 @@ public class QuestionController {
 		// 질문 등록될 때 siteUser 객체를 통해 author_id 저장
 		this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
 		return "redirect:/question/list"; // 질문 저장 후 질문목록으로 이동
+	}
+
+	// 질문수정
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/modify/{id}")
+	public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
+		Question question = this.questionService.getQuestion(id);
+		if (!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+		}
+		// 수정 데이터를 조회해서 보여주기
+		questionForm.setSubject(question.getSubject());
+		questionForm.setContent(question.getContent());
+		return "question_form";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/modify/{id}")
+	public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult,
+			@PathVariable("id") Integer id, Principal principal) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		Question question = this.questionService.getQuestion(id);
+		if (!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+		}
+		this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+		return String.format("redirect:/question/detail/%s", id);
+	}
+
+	// 질문삭제
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/delete/{id}")
+	public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		if (!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+		}
+		this.questionService.delete(question);
+		return "redirect:/";
 	}
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -49,4 +49,15 @@ public class QuestionService {
 		question.setAuthor(author); // 글쓴이 id 저장
 		this.questionRepository.save(question); // question 객체 저장
 	}
+
+	public void modify(Question question, String subject, String content) {
+		question.setSubject(subject);
+		question.setContent(content);
+		question.setModifyDate(LocalDateTime.now());
+		this.questionRepository.save(question);
+	}
+
+	public void delete(Question question) {
+		this.questionRepository.delete(question);
+	}
 }
